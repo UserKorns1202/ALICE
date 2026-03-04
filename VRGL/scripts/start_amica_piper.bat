bb5d819bf3112bc1e2aeb192596124c32693ed1e
Troy Korns <67424835+UserKorns1202@users.noreply.github.com>
2026-03-04 16:47:17 -0500

    Moved VRGL to new folder to separate into new repo
    VRGL is meant to be a portable and easily deployable version of this project, so it will have its own repository to update majorly when things are fleshed out here from my personal version

--- VRGL/scripts/start_amica_piper.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Batch wrapper to launch the PowerShell script in a new window.
-REM This is useful if you want a double-clickable .bat file.
-
-SET SCRIPT_PATH="%~dp0start_amica_piper.ps1"
-REM Use PowerShell to run the launcher script in a new window
-START "AMICA & Piper Launcher" powershell -NoExit -ExecutionPolicy Bypass -File %SCRIPT_PATH%
-
-REM End of file
